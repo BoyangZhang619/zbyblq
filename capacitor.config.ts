0e0291ad0670936743ab82309d83f11b1bbfe51a
ZhangBY619 <boyangzhang246@gmail.com>
2026-04-29 16:14:14 +0800

    初始化init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'xin.zbyblq',
+  appName: 'zbyblq',
+  webDir: 'dist'
+};
+
+export default config;
